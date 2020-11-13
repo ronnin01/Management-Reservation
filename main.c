fc0918d28513946a036c1823eb134b0a6e93fa9c
ronnin01 <gab.github01@gmail.com>
2020-11-13 15:37:39 +0800

    simple added file

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+struct user{
+   char Username[50];
+   char Password[50];
+}user;
 
+int main(){
+    FILE *frecords=fopen("person_records.csv", "r");
+
+    if(frecords == NULL){
+        perror("Unable to open file");
+        exit(1);
+    }
+
+    char line[500];
+
+    while(fgets(line, sizeof(line), frecords)){
+        char *token;
+
+        token = strtok(line,",");
+
+        while(token!=NULL){
+            printf("%s ",token);
+            token = strtok(NULL, ",");
+        }
+        printf("\n");
+    }
 }
